983e45e8214a25ef1716cf567ebfdbc50fbb4800
yasmro <yasmro@users.noreply.github.com>
2026-05-04 14:11:03 +0000

    Deploying to gh-pages from @ yasmro/schatten@04b948d95d7c0f2c345af56b1607e2a8a9e950e8 🚀

---
+++ pr/48/assets/formatter-EIJCOSYU-lhdTHAcK.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-jjckIgrv.js";import{c as t}from"./iframe-FV8AgVr4.js";import"./preload-helper-DT0q8JqK.js";import"./react-18-BqVGTu9R.js";import"./index-zxQO5GXw.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};

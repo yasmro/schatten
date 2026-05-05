0a2caf0d0af32679fe0d52876b994451bd909186
yasmro <yasmro@users.noreply.github.com>
2026-05-05 20:16:23 +0000

    Deploying to gh-pages from @ yasmro/schatten@129c21b7128305a93dcfd3ed82fb4bd3f4a5ff3f 🚀

--- pr/60/assets/formatter-EIJCOSYU-BfEmNqIY.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-D3gFd6xc.js";import{c as t}from"./iframe-BEJAwYOZ.js";import"./preload-helper-BR35kgIy.js";import"./react-18-vomwn8NT.js";import"./index-C3E-b7Qt.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};

f742dcd49d2920620bfee4d704cd3f805776619f
yasmro <30785878+yasmro@users.noreply.github.com>
2026-05-14 00:40:41 +0000

    Deploying to gh-pages from @ yasmro/schatten@7b51dda7c76f2021c802b83db7d709ba4869f180 🚀

---
+++ assets/index-mF_bjEUP.js
@@ -0,0 +1,1 @@
+import{r}from"./iframe-Cqu_LatN.js";function n(t){const e=r.useRef(t);return r.useEffect(()=>{e.current=t}),r.useMemo(()=>(...s)=>{var u;return(u=e.current)==null?void 0:u.call(e,...s)},[])}export{n as u};

31dc12a8108cedc8435d3dfe549f2ab40387538d
yasmro <30785878+yasmro@users.noreply.github.com>
2026-05-31 11:12:43 +0000

    Deploying to gh-pages from @ yasmro/schatten@0724e64f88949dd71a24c189f22ec344f17969e4 🚀

---
+++ next/assets/formatter-EIJCOSYU-3hSxtYaJ.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-BbOq54wC.js";import{c as t}from"./iframe-Dmp1L8uB.js";import"./preload-helper-CrztxVc4.js";import"./react-18-6vY6JT7_.js";import"./index-4jFdP1WL.js";import"./index-sXWsxDeP.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
---
+++ next/assets/index-Bu--PSWt.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-Dmp1L8uB.js";function c(r){const t=e.useRef(r);return e.useEffect(()=>{t.current=r}),e.useMemo(()=>(...u)=>t.current?.(...u),[])}export{c as u};
